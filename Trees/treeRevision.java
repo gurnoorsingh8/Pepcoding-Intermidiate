a9fe91982651df071f59884bf48c3438caca070c
Gurnoor Singh <52324678+gurnoorsingh8@users.noreply.github.com>
2021-06-22 13:19:07 +0530

    Update treeRevision.java

--- Trees/treeRevision.java
+++ Trees/treeRevision.java
@@ -212,6 +212,232 @@ public class treeRevision
 
     //Leetcode 863       distanceK
 
+    public static void preOrderTraversal(Node root, ArrayList<Integer> ans)
+    {
+        if(root == null)
+        {
+            return;
+        }
+
+        ans.add(root.data);
+        preOrderTraversal(root.left, ans);
+        preOrderTraversal(root.right, ans); 
+    }
+
+    public static void inOrderTraversal(Node root, ArrayList<Integer> ans)
+    {
+        if(root == null)
+        {
+            return;
+        }
+
+        inOrderTraversal(root.left, ans);
+        ans.add(root.data);
+        inOrderTraversal(root.right, ans);
+    }
+
+    public static void postOrderTraversal(Node root, ArrayList<Integer> ans)
+    {
+        if(root == null)
+        {
+            return;
+        }
+
+        postOrderTraversal(root.left, ans);
+        postOrderTraversal(root.right, ans);
+        ans.add(root.data);
+    }
+
+    public static int size(Node root)
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+
+        int left = size(root.left);
+        int right = size(root.right);
+        
+        return left + right + 1;
+    }
+
+    public static int height(Node root)
+    {
+        if(root == null)
+        {
+            return -1;
+        }
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+    public static int heightInTermsOfNode(Node root)
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+
+        int left = heightInTermsOfNode(root.left);
+        int right = heightInTermsOfNode(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+    public static int sum(Node root)
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+
+        int left = sum(root.left);
+        int right = sum(root.right);
+
+        return left + right + root.data;
+    }
+
+    public static int maximun(Node root)
+    {
+        if(root == null)
+        {
+            return -(int)1e8;
+        }
+
+        int left = maximum(root.left);
+        int right = maximum(root.right);
+
+        return Math.max(Math.max(left, right), root.data);
+    }
+
+    public static int minimum(Node root)
+    {
+        if(root == null)
+        {
+            return (int)1e8;
+        }
+
+        int left = minimum(root.left);
+        int right = minimum(root.right);
+
+        return Math.min(Math.min(left, right), root.data);
+    }
+
+    public static boolean findData(Node root, int data)
+    {
+        if(root == null)
+        {
+            return false;
+        }
+
+        if(root.data == data)
+        {
+            return true;
+        }
+
+        int left = findData(root.left, data);
+        if(left)
+        {
+            return true;
+        }
+
+        int right = findData(root.right, data);
+        if(right)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void printAtDepthK(Node root, ArrayList<Integer> ans, int k,)
+    {
+        if(root == null || k < 0)
+        {
+            return;
+        }
+
+        if(k == 0)
+        {
+            ans.add(root.data);
+            return;
+        }
+
+        printAtDepthK(root.left, ans, k-1);
+        printAtDepthK(root.right, ans, k - 1);
+    }
+
+    public static void printSingleChildParent(Node root, ArrayList<Integer> ans)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        
+        // parent = root;
+
+        if(root.left == null || root.right == null)
+        {
+            ans.add(root.data);
+        }
+
+        printSingleChildParent(root.left, ans);
+        printSingleChildParent(root.right, ans);
+    }
+
+    public static Node removeLeaves(Node root)
+    {
+        if(root == null)
+        {
+            return null;
+        }
+
+        if(root.left == null && root.right == null)
+        {
+            return null;
+        }
+
+        removeLeaves(root.left);
+        removeLeaves(root.right);
+
+        return root;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
