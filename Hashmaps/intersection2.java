032c994f33de460cf5b0215c0947f67da4d8945e
Gurnoor Singh <52324678+gurnoorsingh8@users.noreply.github.com>
2021-08-11 22:07:23 +0530

    Create intersection2.java

---
+++ Hashmaps/intersection2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class intersection2
+{
+    public static Scanner scn = new Scanner(System.in);
+
+    public static void intersectionWithDuplicates(int[] arr1, int[] arr2)
+    {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int ele : arr1)
+        {
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+        }
+
+        for(int ele : arr2)
+        {
+            if(map.containsKey(ele))
+            {
+                System.out.println(ele);
+                map.put(ele, map.get(ele) - 1);
+                if(map.get(ele) == 0)
+                {
+                    map.remove(ele);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int n = scn.nextInt();
+        int[] arr1 = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            arr1[i] = scn.nextInt();
+        }
+        int m = scn.nextInt();
+        int[] arr2 = new int[m];
+        for(int i = 0; i < m; i++)
+        {
+            arr2[i] = scn.nextInt();
+        }
+        intersectionWithDuplicates(arr1, arr2);
+    }
+}
